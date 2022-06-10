35dfe911129fa9b2efd31b732a42efd9abc7b8e8
ashish8449 <ashishgururani8449@gmail.com>
2022-06-10 10:31:34 +0530

    added delete post feature

--- Server/controllers/post.js
+++ Server/controllers/post.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -18,4 +19,28 @@ export const createPost = async (req, res) => {
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
+};
+
+export const updatePost = async (req, res) => {
+  const { id } = req.params;
+  const { title, message, creator, selectedFile, tags } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
+
+  const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+  await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+  res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
+  await PostMessage.findByIdAndRemove(id);
+  res.json({
+    message: "Post deleted successfully",
+  });
 };
--- Server/routes/post.js
+++ Server/routes/post.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { createPost, getPosts } from "../controllers/post.js";
+import { createPost, deletePost, getPosts, updatePost } from "../controllers/post.js";
 
 const router = express.Router();
 router.get("/", getPosts);
 router.post("/", createPost);
+router.patch("/:id", updatePost);
+router.delete("/:id", deletePost);
 
 export default router;
