90ca24de960412398990e644d2074dae624a2448
ashish8449 <ashishgururani8449@gmail.com>
2022-06-11 15:31:16 +0530

    11june

--- Server/index.js
+++ Server/index.js
@@ -3,14 +3,16 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "../Server/routes/post.js";
+import dotenv from "dotenv";
 const app = express();
+dotenv.config();
 
 // middleware :
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URI = `mongodb+srv://ashish123:tpkfA6tCnF1YzrPV@cluster0.cnqwmch.mongodb.net/?retryWrites=true&w=majority`;
+const CONNECTION_URI = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 // mongodb connection uri
--- Server/routes/post.js
+++ Server/routes/post.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createPost, deletePost, getPosts, updatePost } from "../controllers/post.js";
+import { createPost, deletePost, getPosts, likePost, updatePost } from "../controllers/post.js";
 
 const router = express.Router();
 router.get("/", getPosts);
 router.post("/", createPost);
 router.patch("/:id", updatePost);
 router.delete("/:id", deletePost);
+router.patch('/:id/likePost',likePost);
 
 export default router;
